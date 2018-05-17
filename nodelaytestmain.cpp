2592f7b99e66c4dabb5f5b0397521ae74631c46a
vaughn <vaughncato@gmail.com>
2018-05-17 11:38:37 -0400

    Some refactoring in nodelaytestmain.cpp

--- nodelaytestmain.cpp
+++ nodelaytestmain.cpp
@@ -24,6 +24,66 @@ static void waitForRemoteToClose(Socket &server_socket)
 }
 
 
+static void runServer()
+{
+  Socket listen_socket;
+
+  listen_socket.bindTo(/*port*/1234);
+  listen_socket.startListening(/*backlog*/1);
+
+  Socket server_socket;
+  server_socket.acceptFrom(listen_socket);
+  server_socket.setNoDelay(true);
+
+  for (int i=0; i!=1000; ++i) {
+    char buffer[1] = {'.'};
+    server_socket.send(buffer,sizeof buffer);
+    std::this_thread::sleep_for(5ms);
+  }
+
+  waitForRemoteToClose(server_socket);
+
+  server_socket.close();
+  listen_socket.close();
+}
+
+
+static void runClient(const string &hostname)
+{
+  Socket client_socket;
+  client_socket.connectTo(hostname,1234);
+  char buffer[1];
+  bool first_read = true;
+  std::chrono::high_resolution_clock::time_point last_read_time;
+  using clock = std::chrono::high_resolution_clock;
+
+  for (;;) {
+    ssize_t n_bytes_received =
+      client_socket.recv(buffer,sizeof buffer);
+
+    if (n_bytes_received==0) {
+      break;
+    }
+
+    clock::time_point current_read_time = clock::now();
+
+    if (!first_read) {
+      clock::duration d = current_read_time - last_read_time;
+      int n_ms =
+        std::chrono::duration_cast<std::chrono::milliseconds>(d).count();
+      cerr << "n_ms: " << n_ms << "\n";
+    }
+    else {
+      first_read = false;
+    }
+
+    last_read_time = current_read_time;
+    cerr << buffer[0];
+  }
+  client_socket.close();
+}
+
+
 int main(int argc,char **argv)
 {
   if (argc==1) {
@@ -36,25 +96,7 @@ int main(int argc,char **argv)
   string mode = argv[1];
 
   if (mode=="server") {
-    Socket listen_socket;
-
-    listen_socket.bindTo(/*port*/1234);
-    listen_socket.startListening(/*backlog*/1);
-
-    Socket server_socket;
-    server_socket.acceptFrom(listen_socket);
-    server_socket.setNoDelay(true);
-
-    for (int i=0; i!=1000; ++i) {
-      char buffer[1] = {'.'};
-      server_socket.send(buffer,sizeof buffer);
-      std::this_thread::sleep_for(5ms);
-    }
-
-    waitForRemoteToClose(server_socket);
-
-    server_socket.close();
-    listen_socket.close();
+    runServer();
   }
   else if (mode=="client") {
     if (argc<3) {
@@ -63,38 +105,7 @@ int main(int argc,char **argv)
     }
 
     string hostname = argv[2];
-
-    Socket client_socket;
-    client_socket.connectTo(hostname,1234);
-    char buffer[1];
-    bool first_read = true;
-    std::chrono::high_resolution_clock::time_point last_read_time;
-    using clock = std::chrono::high_resolution_clock;
-
-    for (;;) {
-      ssize_t n_bytes_received =
-        client_socket.recv(buffer,sizeof buffer);
-
-      if (n_bytes_received==0) {
-        break;
-      }
-
-      clock::time_point current_read_time = clock::now();
-
-      if (!first_read) {
-        clock::duration d = current_read_time - last_read_time;
-        int n_ms =
-          std::chrono::duration_cast<std::chrono::milliseconds>(d).count();
-        cerr << "n_ms: " << n_ms << "\n";
-      }
-      else {
-        first_read = false;
-      }
-
-      last_read_time = current_read_time;
-      cerr << buffer[0];
-    }
-    client_socket.close();
+    runClient(hostname);
   }
   else {
     cerr << "Unknown mode: " << mode << "\n";
